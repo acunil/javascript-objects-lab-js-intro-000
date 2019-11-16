ddb0353b4b737b7915403df35de36558cdf4ba07
Lucian Amos <locamos@gmail.com>
2019-11-16 11:50:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ function updateObjectWithKeyAndValue(object, key, value){
   return newObject;
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  var newPair = {[key]:value};
+  object = Object.assign(object, newPair);
+  return object;
+}
