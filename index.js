bf25c76242f0dae078e9e8325a172e264bed21fe
Lucian Amos <locamos@gmail.com>
2019-11-16 11:51:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var recipe = {
 };
 
 function updateObjectWithKeyAndValue(object, key, value){
-  
   var newObject = {[key]:value}
   newObject = Object.assign({}, object, newObject);
   return newObject;
@@ -14,4 +13,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   var newPair = {[key]:value};
   object = Object.assign(object, newPair);
   return object;
+}
+
+function deleteFromObjectByKey(object, key){
+  
 }
